0a96bb4b3d6ebd6d27f11a41be10663cd94107d8
Jacob Logan <jacoblogan98@gmail.com>
2022-03-16 16:34:34 -0400

    commit tree 92d28ed9ff5fec3193e9234ec59c034671185878

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+function findMatching(drivers, string) {
+    const sharedName = drivers.filter(element => element === string || element === string.toLowerCase() || element === string.toUpperCase());
+    return sharedName;
+}
+
+function fuzzyMatch(drivers, string) {
+    const letters = drivers.filter(element => element.slice(0, string.length) === string);
+    return letters;
+}
+
+function matchName(drivers, string) {
+    let name = drivers.filter(element => element.name === string);
+    return name;
+}
